f848c7177222d703ebe51cbdba4a1cc8047cd882
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2023-11-02 11:42:10 +0100

    Fixed ForMathlib/DVR

--- LocalClassFieldTheory/ForMathlib/DiscreteValuationRing.lean
+++ LocalClassFieldTheory/ForMathlib/DiscreteValuationRing.lean
@@ -9,20 +9,19 @@ import Mathlib.RingTheory.DiscreteValuationRing.Basic
   a DVR
 -/
 
-
-theorem RingEquiv.discreteValuationRing {A B : Type _} [CommRing A] [IsDomain A]
-    [DiscreteValuationRing A] [CommRing B] [IsDomain B] (e : A ≃+* B) : DiscreteValuationRing B :=
-  { to_isPrincipalIdealRing := IsPrincipalIdealRing.of_surjective e.toRingHom e.Surjective
-    to_localRing := e.LocalRing
-    not_a_field' :=
-      by
-      have hA : LocalRing.maximalIdeal A ≠ ⊥ := DiscreteValuationRing.not_a_field A
-      obtain ⟨a, ha⟩ := Submodule.nonzero_mem_of_bot_lt (bot_lt_iff_ne_bot.mpr hA)
-      rw [Submodule.ne_bot_iff]
-      use e a
-      constructor
-      · erw [LocalRing.mem_maximalIdeal, map_mem_nonunits_iff (e : A →+* B), ←
-          LocalRing.mem_maximalIdeal]
-        exact a.2
-      · rw [map_ne_zero_iff _ e.injective]
-        simp only [Ne.def, Submodule.coe_eq_zero]; exact ha }
+theorem RingEquiv.discreteValuationRing {A B : Type _} [CommRing A] [IsDomain A] [CommRing B]
+    [IsDomain B] [DiscreteValuationRing A] (e : A ≃+* B) : DiscreteValuationRing B where
+  principal := (isPrincipalIdealRing_iff _).1 <| IsPrincipalIdealRing.of_surjective
+    e.toRingHom e.surjective
+  __ : LocalRing B := e.localRing
+  not_a_field' := by
+    have hA : LocalRing.maximalIdeal A ≠ ⊥ := DiscreteValuationRing.not_a_field A
+    obtain ⟨a, ha⟩ := Submodule.nonzero_mem_of_bot_lt (bot_lt_iff_ne_bot.mpr hA)
+    rw [Submodule.ne_bot_iff]
+    use e a
+    constructor
+    · erw [LocalRing.mem_maximalIdeal, map_mem_nonunits_iff (e : A →+* B), ←
+        LocalRing.mem_maximalIdeal]
+      exact a.2
+    · apply (@map_ne_zero_iff _ _ _ _ e a).2
+      simp only [Ne.def, Submodule.coe_eq_zero]; exact ha
