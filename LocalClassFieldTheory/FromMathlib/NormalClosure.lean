7608cb4f16a1f91505ba7d9c5e13eea8842bb7ff
mariainesdff <mariaines.dff@gmail.com>
2024-01-08 10:45:22 +0100

    port NormalClosure.lean

--- LocalClassFieldTheory/FromMathlib/NormalClosure.lean
+++ LocalClassFieldTheory/FromMathlib/NormalClosure.lean
@@ -3,7 +3,7 @@ Copyright (c) 2023 María Inés de Frutos-Fernández. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: María Inés de Frutos-Fernández
 -/
-import Mathlib.FieldTheory.Normal
+import Mathlib.FieldTheory.NormalClosure
 import Mathlib.FieldTheory.IsAlgClosed.AlgebraicClosure
 
 #align_import from_mathlib.normal_closure
@@ -26,11 +26,13 @@ variable (K L : Type _) [Field K] [Field L] [Algebra K L]
 
 namespace normalClosure
 
+-- Porting note: I am using AlgebraicClosure instead of AlgebraicClosureAux
+
 /-- If `L/K` is an algebraic field extension, then the normal closure of `L/K` in the algebraic
 closure of `L` is an algebraic extension of `K`. -/
 theorem isAlgebraic (h : Algebra.IsAlgebraic K L) :
-    Algebra.IsAlgebraic K (normalClosure K L (AlgebraicClosureAux L)) :=
-  Algebra.isAlgebraic_trans h fun x =>
-    IntermediateField.isAlgebraic_iff.mpr (AlgebraicClosure.isAlgebraic _ _)
+    Algebra.IsAlgebraic K (normalClosure K L (AlgebraicClosure L)) :=
+  Algebra.IsAlgebraic.trans h fun _ =>
+    IntermediateField.isAlgebraic_iff.mpr (AlgebraicClosure.isAlgebraic L _)
 
 end normalClosure
