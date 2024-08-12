0217a4873e50f54cc2f72b8ec4f7f4fd1adc532a
mariainesdff <mariaines.dff@gmail.com>
2024-08-12 10:58:20 +0200

    add PR number

--- LocalClassFieldTheory.lean
+++ LocalClassFieldTheory.lean
@@ -17,7 +17,7 @@ import LocalClassFieldTheory.EqCharacteristic.Basic
 import LocalClassFieldTheory.EqCharacteristic.Valuation
 -- ForMathlib folder *complete*
 -- import LocalClassFieldTheory.ForMathlib.Data.Set.Lattice -- In PR #12961 I got convinced it was useless
-import LocalClassFieldTheory.ForMathlib.FieldTheory.Minpoly.IsIntegrallyClosed
+import LocalClassFieldTheory.ForMathlib.FieldTheory.Minpoly.IsIntegrallyClosed -- In PR #15723
 import LocalClassFieldTheory.ForMathlib.NumberTheory.Padics.PadicIntegers
 -- import LocalClassFieldTheory.ForMathlib.RingTheory.DedekindDomain.Ideal --In PR #13065
 import LocalClassFieldTheory.ForMathlib.RingTheory.Ideal.LocalRing
