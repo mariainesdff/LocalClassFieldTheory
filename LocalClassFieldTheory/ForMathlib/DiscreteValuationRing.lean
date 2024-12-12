c5fd711b36346e0b90d477491ab9a10f870e3c33
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2024-12-12 16:24:48 +0100

    some work on ForMathlib/DVR

--- LocalClassFieldTheory/ForMathlib/DiscreteValuationRing.lean
+++ LocalClassFieldTheory/ForMathlib/DiscreteValuationRing.lean
@@ -9,23 +9,18 @@ import Mathlib.RingTheory.LocalRing.RingHom.Basic
 /-!
 ## Main result
 
-* `ring_equiv.discrete_valuation_ring` shows that if a ring is equivalent to a DVR, it is itself
+* `RingEquiv.discreteValuationRing` shows that if a ring is equivalent to a DVR, it is itself
   a DVR
 -/
 
-theorem RingEquiv.discreteValuationRing {A B : Type _} [CommRing A] [IsDomain A] [CommRing B]
+theorem RingEquiv.discreteValuationRing {A B : Type*} [CommRing A] [IsDomain A] [CommRing B]
     [IsDomain B] [DiscreteValuationRing A] (e : A ≃+* B) : DiscreteValuationRing B where
-  principal := (isPrincipalIdealRing_iff _).1 <| IsPrincipalIdealRing.of_surjective
-    e.toRingHom e.surjective
+  principal := (isPrincipalIdealRing_iff _).1 <| IsPrincipalIdealRing.of_surjective _ e.surjective
   __ : IsLocalRing B := e.isLocalRing
   not_a_field' := by
-    have hA : IsLocalRing.maximalIdeal A ≠ ⊥ := DiscreteValuationRing.not_a_field A
-    obtain ⟨a, ha⟩ := Submodule.nonzero_mem_of_bot_lt (bot_lt_iff_ne_bot.mpr hA)
+    obtain ⟨a, ha⟩ := Submodule.nonzero_mem_of_bot_lt (bot_lt_iff_ne_bot.mpr
+      <| DiscreteValuationRing.not_a_field A)
     rw [Submodule.ne_bot_iff]
-    use e a
-    constructor
-    · erw [IsLocalRing.mem_maximalIdeal, map_mem_nonunits_iff (e : A →+* B), ←
-        IsLocalRing.mem_maximalIdeal]
-      exact a.2
-    · apply (@map_ne_zero_iff _ _ _ _ e a).2
-      simp only [ne_eq, Submodule.coe_eq_zero]; exact ha
+    refine ⟨e a, ⟨?_, by simpa only [map_ne_zero_iff e, ne_eq, Submodule.coe_eq_zero]⟩⟩
+    rw [IsLocalRing.mem_maximalIdeal, map_mem_nonunits_iff e, ← IsLocalRing.mem_maximalIdeal]
+    exact a.2
